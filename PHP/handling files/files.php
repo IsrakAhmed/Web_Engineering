603c656d15b5afaab5d218dec94ed23843eca854
IsrakAhmed <israkahmed7@gmail.com>
2023-09-01 21:53:51 +0600

    practiced html and php file handling in php

---
+++ PHP/handling files/files.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        /*include "header.html";
+
+        echo "this is the site";
+
+        include "footer.html";*/
+    ?> 
+
+    <?php /* include "header.html" */ ?>
+    <!-- <p>Yooooo</p> -->
+    <?php /* include "footer.html" */ ?>
+
+    <?php
+        $name = "israk";
+        $age = 24;
+        include "module.php";
+    ?>
+
+</body>
+</html>
---
+++ PHP/handling files/module.php
@@ -0,0 +1,2 @@
+<h2> <?php echo $name ?> </h2>
+<h4> <?php echo $age ?> </h4>
